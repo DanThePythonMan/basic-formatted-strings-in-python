518918254eb52beffdd34a49ec6cbad5b9a917d1
Daniel <106247186+DanThePythonMan@users.noreply.github.com>
2022-05-26 15:48:31 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+conf1 = "0"
+while conf1=="0":
+    mothername=input("What is your mother's name? ")
+    conf=input(f"Are you sure your mother's name is {mothername} ? ")
+    if conf == "yes":
+        print(mothername)
+        conf="1"
+    else:
+        print("Either you were not understood or you cancelled")
+
+print (mothername)
