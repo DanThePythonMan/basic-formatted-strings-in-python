75f10d572a084f30ae757ad58894a30e05d2da81
Daniel <106247186+DanThePythonMan@users.noreply.github.com>
2022-05-26 15:49:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 conf1 = "0"
 while conf1=="0":
     mothername=input("What is your mother's name? ")
+    #formatted string V
     conf=input(f"Are you sure your mother's name is {mothername} ? ")
     if conf == "yes":
         print(mothername)
